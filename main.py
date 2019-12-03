5f422db7160c37dd67a0b5c427bf249e9aed6058
Nikola Tomic <ntomic@3d4pro.fr>
2019-12-03 12:53:26 -0800

    maybe fixed

--- main.py
+++ main.py
@@ -63,7 +63,11 @@ def create_app(config=None):
     return app
 
 
-if __name__ == "__main__":
+def main(a=None, b=None):
     port = int(os.environ.get("PORT", 8000))
     app = create_app()
     app.run(host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    main()
