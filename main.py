5c876549ad2fae7063b016df4f7a7dd2a0368c46
Nikola Tomic <ntomic@3d4pro.fr>
2019-12-04 15:03:51 -0800

    heroku finally fixed

--- main.py
+++ main.py
@@ -18,54 +18,50 @@ db = client.restfulapi
 score_collection = db.scores
 
 
-def create_app(config=None):
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.config.update(dict(DEBUG=True))
-    app.config.update(config or {})
+app.config.update(dict(DEBUG=True))
+app.config.update({})
 
-    CORS(app)
+CORS(app)
 
-    @app.route("/")
-    def hello_world():
-        return "Hello World"
+@app.route("/")
+def hello_world():
+    return "Hello World"
 
-    @app.route("/score", methods=["POST"])
-    def post_score():
+@app.route("/score", methods=["POST"])
+def post_score():
+    try:
         try:
-            try:
-                body = ast.literal_eval(json.dumps(request.get_json()))
-            except:
-                return "", 400
-            records_fetched = score_collection.find_one({ "name": body["name"] })
-            if records_fetched != None:
-                score_collection.update_one({ "name": body["name"] }, { "$set": { "score": body["score"] } })
-                return "", 201
-            record_created = score_collection.insert(body)
-            if isinstance(record_created, list):
-                return jsonify([str(v) for v in record_created]), 201
-            else:
-                return jsonify(str(record_created)), 201
+            body = ast.literal_eval(json.dumps(request.get_json()))
         except:
-            return "", 500
+            return "", 400
+        records_fetched = score_collection.find_one({ "name": body["name"] })
+        if records_fetched != None:
+            score_collection.update_one({ "name": body["name"] }, { "$set": { "score": body["score"] } })
+            return "", 201
+        record_created = score_collection.insert(body)
+        if isinstance(record_created, list):
+            return jsonify([str(v) for v in record_created]), 201
+        else:
+            return jsonify(str(record_created)), 201
+    except:
+        return "", 500
 
-    @app.route("/score/<user_name>", methods=['GET'])
-    def get_score(user_name):
-        try:
-            records_fetched = score_collection.find_one({ "name": user_name })
-            if records_fetched != None:
-                return dumps(records_fetched)
-            else:
-                return "User not found", 404
-        except:
-            return "", 500
-
-    return app
+@app.route("/score/<user_name>", methods=['GET'])
+def get_score(user_name):
+    try:
+        records_fetched = score_collection.find_one({ "name": user_name })
+        if records_fetched != None:
+            return dumps(records_fetched)
+        else:
+            return "User not found", 404
+    except:
+        return "", 500
 
 
 def main(a=None, b=None):
     port = int(os.environ.get("PORT", 8000))
-    app = create_app()
     app.run(host="0.0.0.0", port=port)
 
 
